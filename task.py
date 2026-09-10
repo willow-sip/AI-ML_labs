b32dcff59881e53857ce8931c7860c43ab41b040
Diana Loyuk <wyrrrth.sip@gmail.com>
2026-09-10 22:11:19 +0300

    feat: split the dataset, taught the linear model

--- task.py
+++ task.py
@@ -1,90 +1,21 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 np.random.seed(42)
 
-# import kagglehub
-# path = kagglehub.dataset_download("prasertk/spotify-global-2019-moststreamed-tracks")
-# print("Path to dataset files:", path)
+df_final = pd.read_csv("processed_spotify_data.csv")
 
-df = pd.read_csv("spotify_global_2019_most_streamed_tracks_audio_features.csv") # point 1
+print("Regression task solving\n\n")
 
-print("First 5 records:\n", df.head()) # point 2
+X_reg = df_final.drop(columns=['energy'])
+y_reg = df_final['energy']
 
-#=================================================================
+# point 1
+X_train_reg, X_test_reg, y_train_reg, y_test_reg = train_test_split(
+    X_reg, y_reg, test_size=0.4, random_state=42
+)
 
-print("Skipped values for each column (before):\n", df.isnull().sum()) # point 3
-print("\nAs seen, there're no missed values, thus we will create some.")
-
-# creating NaN values in random places
-mask_danceability = np.random.rand(len(df)) < 0.05
-mask_mode = np.random.rand(len(df)) < 0.05
-
-df.loc[mask_danceability, 'danceability'] = np.nan
-df.loc[mask_mode, 'mode'] = np.nan
-
-print("\n(NEW)Skipped values for each column (before):\n", df.isnull().sum())
-
-#=================================================================
-
-median_val = df['danceability'].median() # point 4 - filling with mediane
-df['danceability'] = df['danceability'].fillna(median_val)
-print(f"Filled 'danceability' skips with mediane: {median_val}")
-
-mode_val = df['mode'].mode()[0] # point 4 - filling with mode
-df['mode'] = df['mode'].fillna(mode_val)
-print(f"Filled 'mode' skips with mode: {mode_val}")
-
-print("\nSkipped values for each column (after):\n", df.isnull().sum().sum())
-
-
-#=================================================================
-
-features_for_normalization = [ # point 5 - Z-normalization
-    'danceability', 'energy', 'loudness', 'speechiness', 
-    'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'
-]
-
-cols_to_scale = [col for col in features_for_normalization if col in df.columns]
-
-if cols_to_scale:
-    scaler = StandardScaler()
-    
-    df[cols_to_scale] = scaler.fit_transform(df[cols_to_scale])
-    
-    print("Normalization done for columns:", cols_to_scale)
-    print("\nExample of 5 normalized rows:\n")
-    print(df[cols_to_scale].head())
-else:
-    print("No columns for normalization found in dataset")
-   
-#=================================================================
-
-# point 6
-categorical_cols_to_encode = ['mode', 'key', 'time_signature']
-cols_to_encode = [col for col in categorical_cols_to_encode if col in df.columns]
-
-if cols_to_encode:
-    df_encoded = pd.get_dummies(df, columns=cols_to_encode, drop_first=True)
-    
-    print("\nCategorial data formated, new binary columns created:\n")
-    print([col for col in df_encoded.columns if col not in df.columns])
-else:
-    df_encoded = df.copy()
-    print("\nCategorial data to encoe not found")
-
-# making the view more convenient
-cols_to_drop = ['track_id', 'track_name', 'artist_name']
-cols_to_drop = [col for col in cols_to_drop if col in df_encoded.columns]
-
-if cols_to_drop:
-    df_final = df_encoded.drop(columns=cols_to_drop)
-    print(f"\nDeleted text columns with little info: {cols_to_drop}")
-else:
-    df_final = df_encoded
-
-print("\nFinal dataset view:\n", df_final.head())
-print(f"\nFinal dataset size: {df_final.shape[0]} rows and {df_final.shape[1]} columns")
-
-df_final.to_csv("processed_spotify_data.csv", index=False)
+X_test_reg, X_val_reg, y_test_reg, y_val_reg = train_test_split(
+    X_test_reg, y_test_reg, test_size=0.4, random_state=42
+)
