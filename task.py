d0877905b0b0c76191b72df3de6a4cd570050a91
Diana Loyuk <wyrrrth.sip@gmail.com>
2026-09-10 22:22:16 +0300

    fix: improved the model

--- task.py
+++ task.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import (
     mean_squared_error, 
     root_mean_squared_error, 
     mean_absolute_error
 )
-
 
 np.random.seed(42)
 
@@ -16,9 +16,8 @@ df_final = pd.read_csv("processed_spotify_data.csv")
 print("Regression task solving\n")
 
 X_reg = df_final.drop(columns=['energy'])
-X_reg = X_reg.select_dtypes(include=[np.number]) # to not count string-type columns
+X_reg = X_reg.select_dtypes(include=[np.number]) 
 y_reg = df_final['energy']
-
 
 # point 1
 X_train_reg, X_test_reg, y_train_reg, y_test_reg = train_test_split(
@@ -28,6 +27,12 @@ X_train_reg, X_test_reg, y_train_reg, y_test_reg = train_test_split(
 X_test_reg, X_val_reg, y_test_reg, y_val_reg = train_test_split(
     X_test_reg, y_test_reg, test_size=0.4, random_state=42
 )
+
+# scaled for better model results
+scaler = StandardScaler()
+X_train_reg = scaler.fit_transform(X_train_reg)
+X_test_reg = scaler.transform(X_test_reg)
+X_val_reg = scaler.transform(X_val_reg)
 
 # point 2
 lr_model = LinearRegression()
@@ -45,8 +50,8 @@ print(f"-mean squared error:  {mse:.4f}")
 print(f"-root mean squared error: {rmse:.4f}")
 print(f"-mean absolute error:  {mae:.4f}")
 
-ridge_model = Ridge(alpha=1.0)
+ridge_model = Ridge(alpha=1.0, solver='sag', max_iter=2000)
 ridge_model.fit(X_train_reg, y_train_reg)
 y_pred_ridge = ridge_model.predict(X_test_reg)
 rmse_ridge = root_mean_squared_error(y_test_reg, y_pred_ridge)
-print(f"\n Root mean squared error with ridge (L2) regulation: {rmse_ridge:.4f}\n")
+print(f"\nRoot mean squared error with ridge (L2) regulation: {rmse_ridge:.4f}\n")
